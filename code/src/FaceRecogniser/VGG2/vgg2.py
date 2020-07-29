07e9834da0692db4d87d9a3a05afbe3ee18c4d65
JZH <JZH@DESKTOP-2K09L1H>
2020-07-29 16:21:03 +0200

    gui update

--- code/src/FaceRecogniser/VGG2/vgg2.py
+++ code/src/FaceRecogniser/VGG2/vgg2.py
@@ -14,75 +14,30 @@ def predictVGG(face_images, model):
 
 
 def get_vgg_embeddings(face_paths, model):
-    face_list = []
-    face_imgs = []
+    preprocessed_faces, unprocessed_faces = get_faces(face_paths)
+    # VGG
+    vgg_embeddings = predictVGG(preprocessed_faces, model)  # returns embeddings as ndarray of shape (imageCount, embeddingSize)
+    return vgg_embeddings
 
-    # prepare faces for prediction
+def get_vgg_embeddings_and_faces(face_paths, model):
+    preprocessed_faces, unprocessed_faces = get_faces(face_paths)
+    # VGG
+    vgg_embeddings = predictVGG(preprocessed_faces, model)  # returns embeddings as ndarray of shape (imageCount, embeddingSize)
+    return vgg_embeddings, unprocessed_faces
+
+
+# %%
+
+def get_faces(face_paths):
+    processed_faces = []
+    unprocessed_faces = []
     for idx, face_path in enumerate(face_paths):
         # read image
         face = image.load_img(face_path, target_size=(face_size, face_size))
         face = image.img_to_array(face)
-        face_imgs.append(face)
+        unprocessed_faces.append(face)
         face = np.expand_dims(face, axis=0)
         face = utils.preprocess_input(face, version=2)  # or version=2
-        # face = cv2.imread(face_path)
-        # resize Image
-        # face_dim = (face_size, face_size)
-        # resized_face = cv2.resize(face, dsize=face_dim, interpolation=cv2.INTER_CUBIC)
-        face_list.append(np.squeeze(face))
+        processed_faces.append(np.squeeze(face))
+    return processed_faces, unprocessed_faces
 
-    # VGG
-    vgg_embeddings = predictVGG(face_list, model)  # returns embeddings as ndarray of shape (imageCount, embeddingSize)
-
-    return face_imgs, vgg_embeddings
-
-# %%
-
-
-# %%
-# threshold = 0.2
-# clusters = cluster(embeddings.squeeze(), 0.29, 2, "cosine")
-# for label in np.unique(clusters.labels_):
-#     plt.figure(figsize=(20, 10))
-#     columns = 5
-#     face_indices = np.where(clusters.labels_ == label)[0]
-#     print(str(label))
-#     for i, face_idx in enumerate(face_indices):
-#         if i < 5:
-#             plt.subplot(min(5, len(face_indices)) / columns + 1, columns, i + 1)
-#             plt.imshow(faces[face_idx, :, :, :].squeeze().astype(np.uint8))
-#
-#         # %%
-#
-# np.unique(clusters.labels_)
-# cv2.imshow('image', faces[1, :, :, :])
-#
-# # %%
-#
-#
-# accuracies = []
-# recalls = []
-# f1s = []
-# for idx1, embedding1 in enumerate(embeddings):
-#     label = labels_df.loc[labels_df['filename'] == imageNames[idx1]].iat[0, 1]
-#     ground_truth = (labels_df['label'] == label) & (labels_df['filename'] != imageNames[idx1])
-#     predictions = []
-#     for idx2, embedding2 in enumerate(embeddings):
-#         if idx1 != idx2 and cosine(embedding1, embedding2) < threshold:
-#             predictions.append(True)
-#         else:
-#             predictions.append(False)
-#     acc = accuracy_score(predictions, ground_truth)
-#     accuracies.append(acc)
-#     recall = recall_score(predictions, ground_truth)
-#     recalls.append(recall)
-#     f1 = sklearn.metrics.f1_score(predictions, ground_truth);
-#     f1s.append(f1)
-#
-# overall_acc = sum(accuracies) / len(accuracies)
-# overall_recall = sum(recalls) / len(recalls)
-# overall_f1 = sum(f1s) / len(f1s)
-# print('Accuracy: ' + str(overall_acc))
-# print('Recall: ' + str(overall_recall))
-
-# %%
--- code/src/main.py
+++ code/src/main.py
@@ -2,7 +2,7 @@ import os
 from src import Config
 from src.DataLoader import DataLoader
 from src.FaceExtractor.extract_faces import extract_faces
-from src.FaceRecogniser.VGG2.vgg2 import get_vgg_embeddings
+from src.FaceRecogniser.VGG2.vgg2 import get_vgg_embeddings_and_faces
 from src.FaceRecogniser.clustering import evaluate_best_threshold, cluster_predictions
 from src.FotoExtractor.extract_images import extract_images, ExtractionException
 import numpy as np
@@ -45,6 +45,7 @@ def extract_faces_from_folder(image_folder, isAlbum=True):
         extracted_photos_path = os.path.join(image_folder, Config.extracted_photos_path)
         os.makedirs(extracted_photos_path, exist_ok=True)
         for album_page in os.listdir(image_folder):
+            if not album_page.endswith('.tif'): continue
             try:
                 extract_images(os.path.join(image_folder, album_page), extracted_photos_path)
             except ExtractionException:
@@ -61,14 +62,14 @@ def extract_faces_from_folder(image_folder, isAlbum=True):
                       extracted_faces_path)
 
 
-def get_embeddings(folder_path):
+def get_embeddings_and_imgs(folder_path):
     face_paths = []
     folder_path = os.path.join(folder_path, Config.extracted_faces_path)
     for in_filename in os.listdir(folder_path):
         if not in_filename.endswith('.png'): continue
         face_paths.append(os.path.join(folder_path, in_filename))
-    face_imgs, vgg_embeddings = get_vgg_embeddings(face_paths, 'resnet50')
-    return face_imgs, vgg_embeddings
+    vgg_embeddings, face_imgs = get_vgg_embeddings_and_faces(face_paths, 'resnet50')
+    return vgg_embeddings, face_paths
 
 
 if __name__ == '__main__':
